bf4d152e17e42a6acfbbf85268166155fd26ef7c
Shimal Hashimi <166370695+ShimalHashimi@users.noreply.github.com>
2024-10-13 20:28:33 +0530

    ok

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ function showsidenav(){
 function hidesidenav(){
     sidnav.style.left="-45%"
 }
+
+window.addEventListener("scroll", function() {
+    var navbar = document.querySelector('.navbar');
+    var chooseSection = document.querySelector('.impression');
+    var chooseSectionPosition = chooseSection.getBoundingClientRect().top;
+
+    // Check if .choose-section is in the viewport
+    if (chooseSectionPosition <= 0) {
+        navbar.classList.add('white-bg'); // Change background to white
+    } else {
+        navbar.classList.remove('white-bg'); // Restore original background
+    }
+});
